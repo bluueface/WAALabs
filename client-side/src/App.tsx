e4e04a6b8f877340883fc353844d75cf5e4941d2
bluueface <azizwaadoud7@gmail.com>
2024-11-15 14:55:41 -0600

    implement LAB 8

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import { PostProvider } from "./context/PostContext";
 
 function App() {
   return (
     <div className="h-screen p-3 text-white bg-gray-950">
-      <Dashboard></Dashboard>
+      <PostProvider>
+        <Dashboard />
+      </PostProvider>
     </div>
   );
 }
--- client-side/src/components/AddPost.tsx
+++ client-side/src/components/AddPost.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PostInterface, UserInterface } from "../utils/types";
 import { UserService } from "../services/userService";
 import { PostService } from "../services/postService";
@@ -8,62 +8,41 @@ interface Props {
 }
 
 const AddPost: React.FunctionComponent<Props> = ({ setReload }) => {
-  const [post, setPost] = useState<PostInterface>();
-  const [users, setUsers] = useState<any[]>([]);
+  const titleRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLTextAreaElement>(null);
+  const authorRef = useRef<HTMLSelectElement>(null);
+
+  const [users, setUsers] = useState<UserInterface[]>([]);
 
   useEffect(() => {
-    UserService.getAllUsers().then((res: UserInterface[]) => {
+    UserService.getAllUsers().then((res) => {
       setUsers(res);
     });
   }, []);
 
   const handleAddPostButtonClicked = () => {
-    PostService.addPost(post).then((res) => {
+    const newPost: PostInterface = {
+      title: titleRef.current?.value || "",
+      content: contentRef.current?.value || "",
+      user: users.find((user) => user.id === Number(authorRef.current?.value)),
+    };
+
+    PostService.addPost(newPost).then((res) => {
       if (res) {
         setReload(true);
-        setPost(undefined);
+        if (titleRef.current) titleRef.current.value = "";
+        if (contentRef.current) contentRef.current.value = "";
+        if (authorRef.current) authorRef.current.value = "";
       }
     });
   };
 
-  const handleTitleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPost({
-      ...post,
-      title: event.target.value,
-    });
-  };
-  const handleAuthorOnChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    setPost({
-      ...post,
-      user: users.find((user) => user.id == event.target.value),
-    });
-  };
-  const handleContentOnChange = (
-    event: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setPost({
-      ...post,
-      content: event.target.value,
-    });
-  };
-
   return (
-    <div className="p-6 bg-gray-950 border-2 border-gray-500  rounded flex flex-col gap-4">
-      <input
-        placeholder="title"
-        className="px-2 text-black"
-        value={post?.title ?? ""}
-        onChange={handleTitleOnChange}
-      />
-      <select
-        className="px-2 text-black"
-        value={post?.user?.id ?? "--select Author--"}
-        onChange={handleAuthorOnChange}
-      >
-        <option>--select Author--</option>
-        {users?.map((user) => (
+    <div className="p-6 bg-gray-950 border-2 border-gray-500 rounded flex flex-col gap-4">
+      <input placeholder="title" className="px-2 text-black" ref={titleRef} />
+      <select className="px-2 text-black" ref={authorRef}>
+        <option value="">--select Author--</option>
+        {users.map((user: any) => (
           <option key={user.id} value={user.id}>
             {user?.name}
           </option>
@@ -72,12 +51,10 @@ const AddPost: React.FunctionComponent<Props> = ({ setReload }) => {
       <textarea
         className="px-2 text-black"
         placeholder="content"
-        value={post?.content ?? ""}
-        onChange={handleContentOnChange}
+        ref={contentRef}
       />
       <button
-        className={`${!post ? "bg-gray-400" : "bg-green-600"} p-2 rounded`}
-        disabled={!post}
+        className="bg-green-600 p-2 rounded"
         onClick={handleAddPostButtonClicked}
       >
         Add new post
